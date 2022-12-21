46cd9a0395b23713819fd192751f045e6904f235
Leonardo Fabio Da Silva <115595088+LeonardoDaSilva29@users.noreply.github.com>
2022-12-21 20:29:12 -0300

    agregue el modulo saludar y saludar a

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "ejemplo/saludar.html")
+
+def saludar_a(request, nombre):
+    return render (request, 'ejemplo/saludar_a.html',
+    {"nombre": nombre}
+    )
